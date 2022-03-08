25d68aaaf25aeea00ba4b17b00c1aec0d44ed869
MushfigCahatzade <tu7mqdzpy@code.edu.az>
2022-03-08 10:11:38 -0800

    create task 1

---
+++ Task 1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double Sum(double num1, double num2)
+
+
+            {
+                return num1 / num2;
+            }
+            Console.WriteLine(Sum(10,2));
+
+
+        }
+
+    }
+}
