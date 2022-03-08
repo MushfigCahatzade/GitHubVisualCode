7df242c509e93a924be5baa5ae19888c8100ece6
MushfigCahatzade <tu7mqdzpy@code.edu.az>
2022-03-08 11:08:55 -0800

    create task 2

---
+++ Task 2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string Repeat(string word, double a=1)
+
+
+            {
+                return word * a;
+            }
+            Console.WriteLine(Repeat("Hello", 3));
+
+
+        }
+
+    }
+}
